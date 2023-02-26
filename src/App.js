9dac7888e3bdc444e488b0e9505c351c26ea2e64
zessh1 <giorgi.mamulashvili2003@gmail.com>
2023-02-26 16:42:02 +0400

    add: react project "Monster Rolodex"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import {Component, useEffect, useState} from "react";
 import './App.css';
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [searchField, setSearchField] = useState('')
+    const [monsters, setMonsters] = useState([]);
+    const [filterMonsters, setFilterMonsters] = useState(monsters);
+
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((users) => setMonsters(users))
+    },[])
+
+    useEffect(() => {
+        const newFilterdMonsters = monsters.filter((monster) => { return monster.name.toLowerCase().includes(searchField)});
+        setFilterMonsters(newFilterdMonsters)
+    },[monsters,searchField])
+
+
+    const onSearchChange = (event) => {
+        const searchFieldString = event.target.value.toLowerCase()
+        setSearchField(searchFieldString);
+    }
+    return (<div className="App">
+        <h1 className={"app-title"}>Monsters Rolodex</h1>
+        <SearchBox
+            className="search-box"
+            onChangeHandler={onSearchChange}
+            placeholder="search monsters"/>
+        <CardList monsters={filterMonsters}/>
+    </div>)
 }
 
 export default App;
